34bfb3140cbcf4eb1380af565f9a9a876a076b88
Sanup Divakaran <sanukyr@gmail.com>
2025-01-06 22:36:20 +0530

    shallowCopy

--- shallowCopy.js
+++ shallowCopy.js
@@ -1,15 +1,17 @@
-const object1 = {
-  name: "Kumar",
-  details: {
-    age: 44,
-    city: "Shornur",
-    drink: "lemonade",
+const employee = {
+  name: "harish",
+  eID: "MP05S21",
+  profile: {
+    role: "BDM",
+    DOJ: "05-06-2022",
+    Location: "Kannur",
   },
 };
 
-// shallow copy using spread operator
-object1.details.married = true;
-object1.details.city = "Palakkad";
-const obj2 = { ...object1 };
-
-console.log(obj2);
+// shallow copy
+const employeeShallow = { ...employee };
+// console.log("employee", employee);
+// console.log("employeeShallow", employeeShallow);
+employeeShallow.profile.DOJ = "08-06-2022";
+console.log("employee", employee);
+console.log("employeeShallow", employeeShallow);
