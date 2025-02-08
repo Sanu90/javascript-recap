234ddd90813bf1d9b15b3d9845611e920d0d86fe
Sanup Divakaran <sanukyr@gmail.com>
2025-02-08 22:45:40 +0530

    new console program 08Feb

--- 07Feb25.js
+++ 07Feb25.js
@@ -1,5 +1,11 @@
-const str = "hello";
-str.data = "hey";
-console.log(str.data);
+// const str = "hello";
+// str.data = "hey";
+// console.log(str.data);
 
-// undefined as primitive datatypes does not hold any properties.
+// It will print undefined as primitive datatypes does not hold any properties.
+
+let obj = {
+  a: undefined,
+  b: null,
+};
+console.log(JSON.stringify(obj));
