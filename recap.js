a94dc3e115e5690d922f5e403ab16a93aafc0d14
SD <sanukyr@gmail.com>
2025-03-27 19:37:15 +0530

    new workouts

---
+++ recap.js
@@ -0,0 +1,44 @@
+// SPREAD and REST
+// Spread
+// const array=[1,2,3,4];
+// let arr = [8,9,...array]
+// console.log(arr)
+// console.log(...array,...arr);
+
+//  Rest
+
+// const thisArray = [12,3,4,5,6,7]
+// function totalArray(...numbers){
+//     return numbers.reduce((acc,curr)=>  {
+//         return acc+=curr
+//     } ,0)
+// }
+// console.log(totalArray(...thisArray));
+
+//const hello={a:1,b:2,c:3};
+
+// function hey(first,...nextValue){
+//     console.log(first);
+//     return nextValue;
+    
+// }
+// console.log(hey({a:1},{b:2},{c:3}));
+
+//-----------------------------------------------------------------------------------------
+
+// SPLICE & SLICE
+ const array=[1,2,3,4,5,6];
+const size=2;
+const hello=0;
+while(){
+console.log();
+
+}
+
+
+// console.log(array.slice(0,size))
+// console.log(array);
+
+
+
+
