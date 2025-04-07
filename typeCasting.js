b358abf75675fb3c53f6fe50af9d93c6ae4d15ae
SD <sanukyr@gmail.com>
2025-04-07 22:23:06 +0530

    explicit type casting

--- typeCasting.js
+++ typeCasting.js
@@ -3,7 +3,25 @@
 
 // Implicit - It takes place automatically by interpreter or compiler when there is internal requirement
 
-const greet = 'Hai ' + 'Admin';
-console.log(greet);
-const greet1 = 'good ' + 4;
-console.log(greet1);
+// const greet = 'Hai ' + 'Admin';
+// console.log(greet);
+// const greet1 = 'good ' + 4;
+// console.log(greet1);
+// const one=1*'9';
+// console.log(one);
+// const two=1+'2';
+// console.log(two);
+// const three=1-'3';
+// console.log(three);
+
+// Explicit - Converting data type manually by using various built-in methods.
+
+console.log(Number('1'))
+console.log(Number(true));
+
+const newDate = new Date();
+console.log(Number (newDate));
+
+
+
+
