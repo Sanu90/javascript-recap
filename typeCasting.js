c94cca978ae078e631b00a3e0645731b8ff33c29
SD <sanukyr@gmail.com>
2025-04-07 21:56:20 +0530

    typecasting

---
+++ typeCasting.js
@@ -0,0 +1,9 @@
+// Conversion of one data type into another data type is called as type casting.
+// It is of two different types: Inplicit and explicit
+
+// Implicit - It takes place automatically by interpreter or compiler when there is internal requirement
+
+const greet = 'Hai ' + 'Admin';
+console.log(greet);
+const greet1 = 'good ' + 4;
+console.log(greet1);
